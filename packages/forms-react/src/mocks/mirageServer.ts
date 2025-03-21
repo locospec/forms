4c3f7c0ae000f398477eadf7096c823887e4eea3
ShreyDhyani <shrey@betalectic.com>
2025-03-21 16:38:22 +0530

    chore: modified config object and miraj server for examples

--- packages/forms-react/src/mocks/mirageServer.ts
+++ packages/forms-react/src/mocks/mirageServer.ts
@@ -1,7 +1,6 @@
 import { Response, createServer } from "miragejs";
-import { sample_view_config } from "./config";
-import { new_config } from "./newConfig";
 import { person } from "@jsonforms/examples";
+import { SCHEMA, UI_SCHEMA } from "./config";
 
 export function makeServer() {
   const server = createServer({
@@ -34,17 +33,10 @@ export function makeServer() {
                 ],
               },
             },
-            schema: person.schema,
-            uischema: person.uischema,
+            schema: SCHEMA,
+            uischema: UI_SCHEMA,
             initialData: person.data,
           };
-        }
-
-        if (resource === "auction-data" || resource === "auction-data-2") {
-          return sample_view_config;
-        }
-        if (resource === "auction-data-3") {
-          return new_config;
         }
 
         return new Response(404, {}, { message: "Resource not found" });
@@ -76,59 +68,6 @@ export function makeServer() {
             success: true,
             msg: `Record Updated in ${model}`,
             data: body,
-          };
-        }
-
-        return new Response(404, {}, { message: "Resource not found" });
-      });
-
-      // Mocking the /fetch endpoint
-      this.post("/:resource/_read", (_, request) => {
-        const resource = request.params.resource;
-
-        const body = JSON.parse(request.requestBody);
-        const { search, pagination, ...rest } = body;
-        const { cursor } = pagination;
-
-        const pageSize = 10;
-        const filters = JSON.stringify(rest.filters);
-
-        if (
-          resource === "auction-data" ||
-          resource === "auction-data-2" ||
-          resource === "auction-data-3"
-        ) {
-          const completeTestData = Array.from({ length: 200 }, (_, index) => ({
-            id: index + 1,
-            state: `State ${index + 1}`,
-            district: `District ${index + 1}`,
-            cities: `City ${index + 1}`,
-            current_users: Math.floor(Math.random() * 1000),
-            locality: `Locality ${index + 1}`,
-            properties: `Property ${index + 1} ${filters} q=${search}`,
-            "state.id": `state_${index + 1}`,
-          }));
-
-          const paginatedTestData = completeTestData.slice(
-            cursor,
-            cursor + pageSize
-          );
-          const nextCursor =
-            cursor + pageSize < completeTestData.length
-              ? cursor + pageSize
-              : null;
-          const meta = {
-            count: 2,
-            per_page: pageSize,
-            has_more: null,
-            next_cursor: nextCursor,
-            prev_cursor: null,
-          };
-
-          return {
-            success: true,
-            data: paginatedTestData,
-            meta: meta,
           };
         }
 
